e457a7bb0259fce059ac4e44dc4452a7cb0ccd09
Daniel <ingenierosantosd>
2026-04-05 13:10:06 -0700

    Initial Commit

---
+++ SecureDataManager/SecureDataManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SecureDataManagerApp.swift
+//  SecureDataManager
+//
+//  Created by Daniel Santos Mendez on 05/04/26.
+//
+
+import SwiftUI
+
+@main
+struct SecureDataManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SecureDataManagerTests/SecureDataManagerTests.swift
@@ -0,0 +1,17 @@
+//
+//  SecureDataManagerTests.swift
+//  SecureDataManagerTests
+//
+//  Created by Daniel Santos Mendez on 05/04/26.
+//
+
+import Testing
+@testable import SecureDataManager
+
+struct SecureDataManagerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
